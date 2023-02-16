67a49b39f9199f7cfdeceb7ab0a7b39ebf998286
Obie <obiemunozjr@gmail.com>
2023-02-15 19:52:23 -0500

    updated

--- lua/omunoz/set.lua
+++ lua/omunoz/set.lua
@@ -14,8 +14,12 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
 vim.opt.undofile = true
+if os.getenv("HOME") == nil then
+    vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
+else
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+end
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
